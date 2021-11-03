522e7b7b223b185c0e10d4df3893be6a24084e2e
Alfonso Martinez <61892420+ShrikeFound@users.noreply.github.com>
2021-11-03 11:53:18 -0700

    add typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import "./App.css";
+
+const App: FC = () => {
+  const players: string[] = [];
+  return (
+    <div>
+      <h1>Tournament Bracket Tracker</h1>
+    </div>
+  );
+};
+
+export default App;
